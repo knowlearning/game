0ca05d7ba5f581e1a7c0fb10f9b4c5b994ce40b6
Jason Ford <fordjason@gmail.com>
2024-06-24 15:56:54 -0700

    Make paths render properly

--- src/Path.js
+++ src/Path.js
@@ -1,10 +1,14 @@
 import Rapier from '@dimforge/rapier2d-compat'
 
+function distance(a, b) {
+  return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2))
+}
+
 export default class Path {
   constructor(game) {
     this.game = game
     this.path = []
-    this.lineWidth = 4
+    this.lineWidth = 5
 
     const rigidBodyDesc = Rapier.RigidBodyDesc.fixed().setTranslation(0, 0)
     this.rigidBody = game.physics.createRigidBody(rigidBodyDesc)
@@ -15,12 +19,15 @@ export default class Path {
 
     if (this.path.length > 1) {
       const [p1, p2] = this.path.slice(-2)
+      const width = this.lineWidth
+      const height = distance(p1, p2)
       const colliderDesc = (
         Rapier
           .ColliderDesc
-          .cuboid(Math.abs(p1.x - p2.x)/2 + this.lineWidth/2, Math.abs(p1.y - p2.y)/2 + this.lineWidth/2)
+          .cuboid(width/2, height/2)
           .setFriction(0)
           .setRestitution(1)
+          .setRotation(Math.atan2(p1.y-p2.y, p1.x-p2.x) + Math.PI/2)
           .setTranslation((p1.x + p2.x)/2, (p1.y + p2.y)/2)
           .setActiveCollisionTypes(Rapier.ActiveCollisionTypes.ALL)
       )
